b60d299a501553e92dc550e718e6dfd03ac08d9e
Edison Simbaña <edison.simbana@formos.com>
2025-03-19 19:35:07 -0500

    pagina web

---
+++ pagina web/script.js
@@ -0,0 +1,8 @@
+function cambiarMensaje() {
+    document.getElementById('mensaje').innerText = "¡Mensaje cambiado con JavaScript!";
+}
+
+function desactivarBoton() {
+    console.log("Boton desactivado");
+    document.getElementById('boton').disabled = true;
+}
